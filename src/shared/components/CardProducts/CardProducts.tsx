a70a57f8e5b052f9d8b1eba4295d8b24f885ff75
mateoBodiniARG <bodinidev@gmail.com>
2024-07-01 22:36:12 -0300

    feat: se ha agregado los productos al sitio web ya cargados y se han cambiados los estilos

--- src/shared/components/CardProducts/CardProducts.tsx
+++ src/shared/components/CardProducts/CardProducts.tsx
@@ -7,11 +7,58 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-export const CardProduct = () => {
+interface ICardProductProps {
+  title: string;
+  description: string;
+  image: string;
+  price: number;
+  stock: number;
+  color: string;
+}
+
+export const CardProduct: React.FC<ICardProductProps> = ({
+  title,
+  description,
+  image,
+  price,
+  stock,
+  color,
+}) => {
   return (
-    <Card>
-      <CardContent>{/* Imagen del producto */}</CardContent>
-      <CardFooter>{/* Titulo y precio del producto */}</CardFooter>
-    </Card>
+    <div className="rounded-lg shadow-lg overflow-hidden w-full max-w-md flex flex-col">
+      <div>
+        <img
+          src={image}
+          alt="Product Image"
+          width={400}
+          height={300}
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="p-6 flex flex-col flex-grow">
+        <h3 className="text-center text-lg font-bold text-black">{title}</h3>
+        <p className="text-gray-600 text-sm font-semibold text-center mt-2 flex-grow">
+          {description}
+        </p>
+        <hr className="mt-2" />
+        <div className="flex items-center justify-between mt-4">
+          <div className="grid gap-1">
+            <p className="text-sm font-medium text-gray-600">Precio</p>
+            <p className="text-lg font-semibold text-black">${price}</p>
+          </div>
+          <div className="grid gap-1">
+            <p className="text-sm font-medium text-gray-600">Stock</p>
+            <p className="text-lg font-semibold text-black">{stock}</p>
+          </div>
+          <div className="grid gap-1">
+            <p className="text-sm font-medium text-gray-600">Color</p>
+            <p className="text-lg font-semibold text-black">{color}</p>
+          </div>
+        </div>
+        <button className="mt-4 w-full bg-black rounded-md py-1.5 text-white font-semibold hover:bg-zinc-700 transition duration-300 ease-in-out">
+          Agregar a carrito
+        </button>
+      </div>
+    </div>
   );
 };
--- src/shared/components/GridProducts/GridProducts.tsx
+++ src/shared/components/GridProducts/GridProducts.tsx
@@ -1,10 +1,53 @@
+'use client'
+import { useEffect, useState } from "react";
+import { db } from "@/firebase/config";
+import { collection, getDocs } from "firebase/firestore";
 import { CardProduct } from "../CardProducts/CardProducts";
 
+interface IProduct {
+  title: string;
+  description: string;
+  image: string;
+  price: number;
+  stock: number;
+  color: string;
+}
+
 export const GridProducts = () => {
+  const [products, setProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "celulares-apple"));
+        const productsData: IProduct[] = [];
+
+        querySnapshot.forEach((doc) => {
+          const product = doc.data() as IProduct;
+          productsData.push(product);
+        });
+
+        setProducts(productsData);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <div className="mt-4 w-full h-full grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((x, i) => (
-        <CardProduct key={i} />
+      {products.map((product, index) => (
+        <CardProduct
+          key={index}
+          title={product.title}
+          description={product.description}
+          image={product.image}
+          price={product.price}
+          stock={product.stock}
+          color={product.color}
+        />
       ))}
     </div>
   );
