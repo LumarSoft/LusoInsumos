f7c7628378ffcff7773430e71217cf82222a339a
marcebenitez2 <marcebenitez0607@gmail.com>
2024-08-19 21:48:35 -0300

    feat: cambio en next.config

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/getProductsEditables",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, no-cache, must-revalidate, proxy-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- src/app/(sidebar)/admin/page.tsx
+++ src/app/(sidebar)/admin/page.tsx
@@ -1,13 +1,22 @@
+// src/app/(sidebar)/admin/page.tsx
 import AdminModule from "@/modules/Admin/home";
+import { BASE_API_URL } from "@/shared/providers/envProvider";
 import { IProductManual } from "@/shared/types/IProductManual";
 
 export default async function Admin() {
-  const response = await fetch(`https://www.lusoinsumos.com/api/getProductsEditables`, {
-    cache: "no-cache",
-  });
+  try {
+    const response = await fetch(`${BASE_API_URL}/getProductsEditables`, {
+      cache: "no-store",
+    });
 
-  const products: IProductManual[] = await response.json();
-  console.log(products);
+    if (!response.ok) {
+      throw new Error(`Error al obtener productos: ${response.statusText}`);
+    }
 
-  return <AdminModule products={products} />;
+    const products: IProductManual[] = await response.json();
+    return <AdminModule products={products} />;
+  } catch (error) {
+    console.error("Error al cargar productos:", error);
+    return <div>Error al cargar productos</div>; // Manejo de errores en la UI
+  }
 }
