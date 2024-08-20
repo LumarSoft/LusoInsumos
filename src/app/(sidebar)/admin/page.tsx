76d80e4946af2736f74ca125a532cbdb50ad3b1b
marcebenitez2 <marcebenitez0607@gmail.com>
2024-08-19 21:10:58 -0300

    feat: ultimo

--- src/app/(sidebar)/admin/page.tsx
+++ src/app/(sidebar)/admin/page.tsx
@@ -1,13 +1,11 @@
 import AdminModule from "@/modules/Admin/home";
+import { BASE_API_URL } from "@/shared/providers/envProvider";
 import { IProductManual } from "@/shared/types/IProductManual";
 
 export default async function Admin() {
-  const response = await fetch(
-    "https://www.lusoinsumos.com/api/getProductsEditables",
-    {
-      next: { revalidate: 0 },
-    }
-  );
+  const response = await fetch(`${BASE_API_URL}/getProductsEditables`, {
+    cache: "no-cache",
+  });
 
   const products: IProductManual[] = await response.json();
   console.log(products);
