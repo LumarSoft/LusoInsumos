53b0e4b47e79fc85a0db9d00a83946a1dfcadf8c
marcebenitez2 <marcebenitez0607@gmail.com>
2024-08-20 19:44:40 -0300

    feat: cambio en el page de sidebar/admin

--- src/app/(sidebar)/admin/page.tsx
+++ src/app/(sidebar)/admin/page.tsx
@@ -4,17 +4,8 @@ import { fetchData } from "@/services/request";
 import { IProductManual } from "@/shared/types/IProductManual";
 
 export default async function Admin() {
-  try {
-    const response = await fetchData("getProductsEditables")    
 
-    if (!response.ok) {
-      throw new Error(`Error al obtener productos: ${response.statusText}`);
-    }
+  const products = await fetchData("getProductsEditables");
 
-    const products: IProductManual[] = await response.json();
-    return <AdminModule products={products} />;
-  } catch (error) {
-    console.error("Error al cargar productos:", error);
-    return <div>Error al cargar productos</div>; // Manejo de errores en la UI
-  }
+  return <AdminModule products={products as IProductManual[]} />;  
 }
