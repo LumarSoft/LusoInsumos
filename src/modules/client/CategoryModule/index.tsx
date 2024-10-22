cc6a9effbbd3001a252d0fce569503ca28b1c233
mateoBodiniARG <bodinidev@gmail.com>
2024-10-22 12:51:56 -0300

    feat: Card producto no encontrado

--- src/modules/client/CategoryModule/index.tsx
+++ src/modules/client/CategoryModule/index.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { GridProducts } from "@/shared/components/GridProducts/GridProducts";
 import { IProduct } from "@/shared/types/IProduct";
+import { PackageSearch } from "lucide-react";
 
 interface CategoryModuleProps {
   products: IProduct[];
@@ -12,13 +21,32 @@ export default function CategoryModule({
   category,
   brands,
 }: CategoryModuleProps) {
-
   return (
     <div className="px-4 md:px-28 2xl:px-80">
       {products.length > 0 ? (
         <GridProducts products={products} brands={brands} category={category} />
       ) : (
-        <h3 className="text-center">No se encontraron productos</h3>
+        <Card className="w-full max-w-md mx-auto mt-20">
+          <CardHeader className="text-center">
+            <div className="w-20 h-20 mx-auto bg-muted rounded-full flex items-center justify-center">
+              <PackageSearch className="h-10 w-10 text-muted-foreground" />
+            </div>
+            <CardTitle className="mt-4 text-xl font-semibold">
+              No se han encontrado productos
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="text-center text-muted-foreground">
+            <p>
+              Lo sentimos, no hemos podido encontrar ningún producto que
+              coincida con tu búsqueda.
+            </p>
+          </CardContent>
+          <CardFooter className="flex justify-center">
+            <Button variant="outline">
+              <a href="/">Regresar a la home</a>
+            </Button>
+          </CardFooter>
+        </Card>
       )}
     </div>
   );
